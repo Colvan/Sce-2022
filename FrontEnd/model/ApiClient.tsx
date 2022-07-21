9bb9da81e3d7423cdd0a8e788c0fab170d9f6cbd
Omri Biton <fighterisr@gmail.com>
2022-07-21 07:18:49 +0300

    changed the ApiClient.tsx baseUrl ip to 10.0.2.2 which is the alias for the host machine ip for development purposes

--- FrontEnd/model/ApiClient.tsx
+++ FrontEnd/model/ApiClient.tsx
@@ -2,7 +2,7 @@ import { create } from "apisauce"
 
 
 const apiClient = create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://10.0.2.2:3000',
     headers: { Accept: 'application/vnd.github.v3+json' },
  })
  

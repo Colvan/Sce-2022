f0198978bec46b8ec2cc7994120659274ea45405
colvan <yoniborisenco@gmail.com>
2022-07-31 18:29:59 +0300

    create chat
    create chat demo with constant message
    will be replaced with live

--- FrontEnd/src/screens/chat_screen.tsx
+++ FrontEnd/src/screens/chat_screen.tsx
@@ -1,53 +1,179 @@
-import React, {FC, useEffect, useRef, useState} from "react"
-import {View, Text} from "react-native"
-import {NavigationProps} from "../AppEntry";
-import io, {Socket} from 'socket.io-client';
+import React, { FC, useEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableHighlight,
+  TextInput,
+} from "react-native";
+import { NavigationProps } from "../AppEntry";
+import io, { Socket } from "socket.io-client";
 import store from "../store/store";
+import COLORS from "../constants/colors";
 
 type messageType = {
-    message: string,
-    time: string,
-    from: string
-}
+  message: string;
+  time: string;
+  from: string;
+};
 
-const Chat: FC<NavigationProps> = ({navigation, route}) => {
-    const [messages, setMessages] = useState<Array<messageType>>([]);
-    const socketRef = useRef<Socket>();
-    const userToken = store.getState().auth.userToken
-    const date = new Date()
-
-    useEffect(() => {
-        socketRef.current = io('http://10.0.2.2:3000', {
-            auth: {
-                token: 'bearer ' + userToken!.access_token
-            }
-        });
-        socketRef.current?.on('connect', () => {
-            console.log("client connected");
-        })
-        // socketRef.current?.on('message', (message) => {
-
-        // });
-
-        socketRef.current?.on("ims:message_to_all", (message: messageType)  => {
-            setMessages([...messages, message]);
-            console.log(`got this message: ${message.message} on time: ${message.time}, from: ${message.from}`)
-        })
-        // socketRef.current?.emit("message",{message: "hello!", time: date.toLocaleTimeString(), from: userToken!.email})
-
-
-        return () => {
-            socketRef.current?.disconnect();
-        }
-    }, [])
-
-
-
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Chat</Text>
+type MessageListRowProps = {
+  message: messageType;
+  currentUser: String;
+};
+const MessageListRow: FC<MessageListRowProps> = ({ message, currentUser }) => {
+  return (
+    <TouchableHighlight underlayColor={COLORS.clickBackground}>
+      <View style={styles.list_row_container}>
+        <View style={styles.list_row_text_container}>
+          <Text style={styles.list_row_id}>
+            {currentUser == message.from && (
+              <Text style={styles.from_message_style}>{message.from}</Text>
+            )}
+            {currentUser != message.from && (
+              <Text style={styles.list_row_name}>{message.from}</Text>
+            )}
+          </Text>
+          <Text style={styles.list_row_name}>{message.message}</Text>
         </View>
-    )
-}
+      </View>
+    </TouchableHighlight>
+  );
+};
 
-export default Chat
+const Chat: FC<NavigationProps> = ({ navigation, route }) => {
+  const [messages, setMessages] = useState<Array<messageType>>([]);
+  const [DemoMessages, setDemoMessages] = useState<Array<messageType>>([]);
+
+  const socketRef = useRef<Socket>();
+  const userToken = store.getState().auth.userToken;
+  const currentUser = userToken!.email;
+  const date = new Date();
+
+  var demoConversationArray = [
+    { from: "omri biton", message: "hello im omri", time: "today" },
+    { from: "ivan@test.com", message: "hello omri", time: "today" },
+  ];
+
+  useEffect(() => {
+    setDemoMessages(demoConversationArray);
+    socketRef.current = io("http://10.0.2.2:3000", {
+      auth: {
+        token: "bearer " + userToken!.access_token,
+      },
+    });
+    socketRef.current?.on("connect", () => {
+      console.log("client connected");
+    });
+    // socketRef.current?.on('message', (message) => {
+
+    // });
+
+    socketRef.current?.on("ims:message_to_all", (message: messageType) => {
+      setMessages([...messages, message]);
+      console.log(
+        `got this message: ${message.message} on time: ${message.time}, from: ${message.from}`
+      );
+    });
+    // socketRef.current?.emit("message",{message: "hello!", time: date.toLocaleTimeString(), from: userToken!.email})
+
+    return () => {
+      socketRef.current?.disconnect();
+    };
+  }, []);
+
+  const onSendMessage = async () => {};
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Chat</Text>
+      <FlatList
+        data={DemoMessages}
+        keyExtractor={(item) => item.message.toString()}
+        renderItem={({ item }) => (
+          <MessageListRow message={item} currentUser={currentUser} />
+        )}
+      ></FlatList>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Message"
+          keyboardType="default"
+        ></TextInput>
+        <TouchableHighlight
+          onPress={onSendMessage}
+          underlayColor={COLORS.clickBackground}
+          style={styles.button}
+        >
+          <Text style={styles.button_text}>Send</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "grey",
+    width: "70%",
+  },
+  button: {
+    margin: 12,
+    backgroundColor: "grey",
+    borderRadius: 5,
+    width: "20%",
+  },
+  button_text: {
+    fontSize: 30,
+    color: "white",
+    textAlign: "center",
+    marginTop: 3,
+    marginBottom: 3,
+  },
+  touchablebutton: {
+    width: 40,
+  },
+  home_container: {
+    flex: 1,
+  },
+  from_message_style: {
+    backgroundColor: "grey",
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  list_row_container: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "white",
+    flexDirection: "row",
+    elevation: 4,
+    borderRadius: 3,
+    marginLeft: 6,
+    marginRight: 8,
+  },
+  list_row_text_container: {
+    justifyContent: "center",
+    width: "100%",
+  },
+  list_row_name: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  list_row_id: {
+    fontSize: 25,
+  },
+  activity_indicator: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+  },
+});
+
+export default Chat;
--- FrontEnd/src/screens/home_screen.tsx
+++ FrontEnd/src/screens/home_screen.tsx
@@ -1,137 +1,144 @@
-import React, { FC, useEffect, useState } from "react"
-import { View, Text, Image, StyleSheet, FlatList, TouchableHighlight } from "react-native"
-import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { FC, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableHighlight,
+} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import COLORS from "../constants/colors"
-import StudentModel,{Post} from "../model/student_model"
-import ActivityIndicator from "./component/custom_activity_indicator"
-import {NavigationProps} from "../AppEntry";
-import {AuthActions} from "../store/authSlice";
+import COLORS from "../constants/colors";
+import StudentModel, { Post } from "../model/student_model";
+import ActivityIndicator from "./component/custom_activity_indicator";
+import { NavigationProps } from "../AppEntry";
+import { AuthActions } from "../store/authSlice";
 
 import Credentials from "../utils/credentials";
-import {useAppDispatch} from "../store/storeHooks";
+import { useAppDispatch } from "../store/storeHooks";
 import store from "../store/store";
 
-
-
 type StudentListRowProps = {
-    post: Post
-}
-
-
-
+  post: Post;
+};
 
 const StudentListRow: FC<StudentListRowProps> = ({ post }) => {
-    return (
-        <TouchableHighlight
-            underlayColor={COLORS.clickBackground}>
-            <View style={styles.list_row_container}>
-                { post.imageUrl != "" &&  <Image source={{uri: post.imageUrl.toString()}} style={styles.list_row_image}></Image>}
-                { post.imageUrl == "" &&  <Image source={require("../../assets/avatar.jpeg")} style={styles.list_row_image}></Image>}
-                <View style={styles.list_row_text_container}>
-                    <Text style={styles.list_row_id}>{post.id}</Text>
-                    <Text style={styles.list_row_name}>{post.message}</Text>
-                </View>
-            </View>
-        </TouchableHighlight>
-    )
-}
+  return (
+    <TouchableHighlight underlayColor={COLORS.clickBackground}>
+      <View style={styles.list_row_container}>
+        {post.imageUrl != "" && (
+          <Image
+            source={{ uri: post.imageUrl.toString() }}
+            style={styles.list_row_image}
+          ></Image>
+        )}
+        {post.imageUrl == "" && (
+          <Image
+            source={require("../../assets/avatar.jpeg")}
+            style={styles.list_row_image}
+          ></Image>
+        )}
+        <View style={styles.list_row_text_container}>
+          <Text style={styles.list_row_id}>{post.id}</Text>
+          <Text style={styles.list_row_name}>{post.message}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
 
-const LogOutButton: FC<{ onClick: () => void }> = ({onClick}) => {
-    return (
-        <TouchableHighlight style = {styles.touchablebutton} onPress={() => {
-            onClick()
-        }}
-                            underlayColor={COLORS.clickBackground}>
-            <Ionicons name={"log-out-outline"} size={40}  color={'gray'}/>
-        </TouchableHighlight>
-    )
-}
+const LogOutButton: FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <TouchableHighlight
+      style={styles.touchablebutton}
+      onPress={() => {
+        onClick();
+      }}
+      underlayColor={COLORS.clickBackground}
+    >
+      <Ionicons name={"log-out-outline"} size={40} color={"gray"} />
+    </TouchableHighlight>
+  );
+};
 
 const Home: FC<NavigationProps> = ({ navigation, route }) => {
-    const [data, setData] = useState<Array<Post>>()
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const dispatch = useAppDispatch();
+  const [data, setData] = useState<Array<Post>>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    navigation.addListener("focus", () => {
+      reloadData();
+    });
+  }, [navigation]);
 
+  const reloadData = async () => {
+    setIsLoading(true);
+    const studentData = await StudentModel.getAllPosts();
+    setData(studentData);
+    setIsLoading(false);
+  };
 
+  const logOut = async () => {
+    await Credentials.setCredentials(null);
+    dispatch(AuthActions.logOut());
+  };
 
-
-    useEffect(()=>{
-        navigation.addListener('focus',()=>{
-            reloadData()
-        })
-    },[navigation])
-
-    const reloadData = async ()=>{
-        setIsLoading(true)
-        const studentData = await StudentModel.getAllPosts()
-        setData(studentData)
-        setIsLoading(false)
-    }
-
-    const logOut = async () => {
-        await Credentials.setCredentials(null);
-        dispatch(AuthActions.logOut());
-    }
-
-    
-    return (
-        <View style={styles.home_container}>
-            <LogOutButton onClick={logOut} ></LogOutButton>
-            <FlatList
-                data={data}
-                keyExtractor={item => item.postId.toString()}
-                renderItem={({ item }) => (<StudentListRow post={item}/>)}
-            ></FlatList>
-            <View style={styles.activity_indicator}>
-                <ActivityIndicator visible={isLoading}></ActivityIndicator>
-            </View>
-        </View>
-    )
-}
-
+  return (
+    <View style={styles.home_container}>
+      <LogOutButton onClick={logOut}></LogOutButton>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.postId.toString()}
+        renderItem={({ item }) => <StudentListRow post={item} />}
+      ></FlatList>
+      <View style={styles.activity_indicator}>
+        <ActivityIndicator visible={isLoading}></ActivityIndicator>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    touchablebutton:{
-        width:40
-        
-    },
-    home_container: {
-        flex: 1
-    },
-    list_row_container: {
-        height: 150,
-        // width: "100%",
-        // backgroundColor: "grey",
-        flexDirection: "row",
-        elevation: 4,
-        borderRadius: 3,
-        marginLeft: 6,
-        marginRight: 8
-    },
-    list_row_image: {
-        height: 130,
-        width: 100,
-        margin: 10,
-        borderRadius: 15
-    },
-    list_row_text_container: {
-        justifyContent: "center"
-    },
-    list_row_name: {
-        fontSize: 16,
-        marginBottom: 10
-    },
-    list_row_id: {
-        fontSize: 25
-    },
-    activity_indicator:{
-        width: "100%",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute"
-    }
-})
-export default Home
+  touchablebutton: {
+    width: 40,
+  },
+  home_container: {
+    flex: 1,
+  },
+  list_row_container: {
+    height: 150,
+    // width: "100%",
+    // backgroundColor: "grey",
+    flexDirection: "row",
+    elevation: 4,
+    borderRadius: 3,
+    marginLeft: 6,
+    marginRight: 8,
+  },
+  list_row_image: {
+    height: 130,
+    width: 100,
+    margin: 10,
+    borderRadius: 15,
+  },
+  list_row_text_container: {
+    justifyContent: "center",
+  },
+  list_row_name: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  list_row_id: {
+    fontSize: 25,
+  },
+  activity_indicator: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+  },
+});
+export default Home;
